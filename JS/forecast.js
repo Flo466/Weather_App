ec3983bcef02f162da5a7aa5c3e96416efea74ca
Flo2503 <bruel.florent@gmail.com>
2025-01-08 18:51:10 +0100

    changed port

--- JS/forecast.js
+++ JS/forecast.js
@@ -2,7 +2,7 @@ import { ajaxRequest } from './request.js';
 
 export async function getForecast(city) {
     // L'URL pointe maintenant vers ton serveur backend, pas l'API météo directement
-    const url = `http://localhost:3000/forecast?city=${encodeURIComponent(city)}`;
+    const url = `http://localhost:3001/forecast?city=${encodeURIComponent(city)}`;
     try {
         const forecast = await ajaxRequest({ url, method: 'GET' });
         console.log('Météo récupérée:', forecast);

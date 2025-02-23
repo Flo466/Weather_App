71ce7938e596e71e5350a72d46729c06017d10eb
Flo466 <bruel.florent@gmail.com>
2025-02-23 02:14:03 +0100

    update icons

--- src/data.js
+++
@@ -1,9 +0,0 @@
-// Détails météo
-export const weatherDetails = [
-    { iconSrc: 'asset/sun.png', label: 'Max / Min', value: '32 / 20' },
-    { iconSrc: 'asset/sun.png', label: 'Coucher du soleil', value: '18:45' },
-    { iconSrc: 'asset/sun.png', label: "Ensoleillement", value: '16h' },
-    { iconSrc: 'asset/temp.png', label: 'Index UV', value: '7' },
-    { iconSrc: 'asset/hydro.png', label: 'Temps de pluie', value: '0' },
-    { iconSrc: 'asset/wind.png', label: 'Vent', value: '15 km/h' },
-];
--- src/utils/data.js
+++ src/utils/data.js
@@ -1,8 +1,8 @@
 export const weatherDetails = [
-    { iconSrc: 'assets/sun.png', label: 'Max / Min', value: ''},
-    { iconSrc: 'assets/sun.png', label: 'Coucher du soleil', value: ''},
-    { iconSrc: 'assets/sun.png', label: "Ressenti", value: ''},
-    { iconSrc: 'assets/temp.png', label: 'Index UV', value: ''},
+    { iconSrc: 'assets/temperature.png', label: 'Max / Min', value: ''},
+    { iconSrc: 'assets/sunset.png', label: 'Coucher du soleil', value: ''},
+    { iconSrc: 'assets/thermostat.png', label: "Ressenti", value: ''},
+    { iconSrc: 'assets/uv.png', label: 'Index UV', value: ''},
     { iconSrc: 'assets/hydro.png', label: 'Humidité', value: ''},
     { iconSrc: 'assets/wind.png', label: 'Vent', value: ''},
 ];
